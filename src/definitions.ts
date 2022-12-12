f9d04f2310222e7bbfdc40204610d7c56286b1a0
Kelvin Koh <kelvin.koh@prometheanworld.com>
2022-12-12 11:50:35 +0000

    working promethean plugin

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,9 @@
 export interface ApiPluginPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+  openMap(options: OpenMapOptions): Promise<OpenMapOptions>;
+}
+
+export interface OpenMapOptions {
+  latitude: number;
+  longitude: number;
 }
--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,17 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { ApiPluginPlugin } from './definitions';
+import type {
+  ApiPluginPlugin,
+  OpenMapOptions
+} from './definitions';
 
 export class ApiPluginWeb extends WebPlugin implements ApiPluginPlugin {
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
+  }
+
+  async openMap(location: OpenMapOptions): Promise<OpenMapOptions> {
+    return location;
   }
 }
